b6263bc2b04f83c09ecb5074e7e96aa340cafaea
luanqian <luanqian@douban.com>
2017-11-24 14:09:43 +0800

    fix : MD5方法不是线程安全的

--- core/src/main/java/com/douban/rexxar/utils/MD5Utils.java
+++ core/src/main/java/com/douban/rexxar/utils/MD5Utils.java
@@ -8,23 +8,13 @@ import java.security.MessageDigest;
 
 public class MD5Utils {
 
-    private static MessageDigest md5 = null;
-
-    static {
-        try {
-            md5 = MessageDigest.getInstance("MD5");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
     public static String getMd5(String str) {
         return getMd5(str.getBytes());
     }
 
     public static String getMd5(byte[] bytes) {
         try {
-            byte[] bs = md5.digest(bytes);
+            byte[] bs = MessageDigest.getInstance("MD5").digest(bytes);
             StringBuilder sb = new StringBuilder(40);
             for (byte x : bs) {
                 if ((x & 0xff) >> 4 == 0) {
